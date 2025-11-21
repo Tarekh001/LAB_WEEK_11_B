60ae1219964f15aaf3139c8abd76c3aa1178d538
Muhammad Tarekh <m.tarekh2004@gmail.com>
2025-11-21 11:36:39 +0700

    Commit No. 2: Photo and Video using FileProvider and MediaStore

--- app/src/main/java/com/example/lab_week_11_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11_b/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.lab_week_11_b
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_EXTERNAL_STORAGE = 3
+    }
+
+    // Helper class to manage files in MediaStore
+    private lateinit var providerFileManager: ProviderFileManager
+    // Data model for the file
+    private var photoInfo: FileInfo? = null
+    private var videoInfo: FileInfo? = null
+    // Flag to indicate whether the user is capturing a photo or video
+    private var isCapturingVideo = false
+    // Activity result launcher to capture images and videos
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
+    private lateinit var takeVideoLauncher: ActivityResultLauncher<Uri>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +40,110 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        providerFileManager =
+            ProviderFileManager(
+                applicationContext,
+                FileHelper(applicationContext),
+                contentResolver,
+                Executors.newSingleThreadExecutor(),
+                MediaContentHelper()
+            )
+
+        takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                providerFileManager.insertImageToStore(photoInfo)
+            }
+        }
+
+        takeVideoLauncher = registerForActivityResult(ActivityResultContracts.CaptureVideo()) { success ->
+            if (success) {
+                providerFileManager.insertVideoToStore(videoInfo)
+            }
+        }
+
+        findViewById<Button>(R.id.photo_button).setOnClickListener {
+            // Set the flag to indicate that the user is capturing a photo
+            isCapturingVideo = false
+            // Check the storage permission
+            // If the permission is granted, open the camera
+            // Otherwise, request the permission
+            checkStoragePermission {
+                openImageCapture()
+            }
+        }
+        findViewById<Button>(R.id.video_button).setOnClickListener {
+            // Set the flag to indicate that the user is capturing a video
+            isCapturingVideo = true
+            // Check the storage permission
+            // If the permission is granted, open the camera
+            // Otherwise, request the permission
+            checkStoragePermission {
+                openVideoCapture()
+            }
+        }
+    }
+
+    private fun openImageCapture() {
+        val info = providerFileManager.generatePhotoUri(System.currentTimeMillis())
+        photoInfo = info
+        takePictureLauncher.launch(info.uri)
+    }
+
+    private fun openVideoCapture() {
+        val info = providerFileManager.generateVideoUri(System.currentTimeMillis())
+        videoInfo = info
+        takeVideoLauncher.launch(info.uri)
+    }
+
+    private fun checkStoragePermission(onPermissionGranted: () -> Unit) {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q) {
+            //Check for the WRITE_EXTERNAL_STORAGE permission
+            when (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )) {
+                // If the permission is granted
+                PackageManager.PERMISSION_GRANTED -> {
+                    onPermissionGranted()
+                }
+                // if the permission is not granted, request the permission
+                else -> {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        REQUEST_EXTERNAL_STORAGE
+                    )
+                }
+            }
+        } else {
+            onPermissionGranted()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            //Check if requestCode is for the External Storage permission or not
+            REQUEST_EXTERNAL_STORAGE -> {
+                // If granted, open the camera
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    if (isCapturingVideo) {
+                        openVideoCapture()
+                    } else {
+                        openImageCapture()
+                    }
+                }
+                return
+            }
+            // for other request code, do nothing
+            else -> {
+            }
         }
     }
 }
